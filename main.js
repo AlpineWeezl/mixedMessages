24310b7032cda6bdc983cd5c5a28ca2b704153f6
Matthias Wieser <matthias.wieser.83@live.de>
2022-04-04 10:15:10 +0200

    created shuffle function

---
+++ main.js
@@ -0,0 +1,7 @@
+const adjectives = ['best', 'smartest', 'sweetest', 'most awesome'];
+const nomen = ['coder', 'programmer', 'hacker'];
+
+function shuffleWords(adjectivesLength, nomenLength) {
+    const randomAdjectiveIndex = Math.floor(Math.random() * 4);
+    const randomNomenIndex = Math.floor(Math.random() * 3);
+    return [randomAdjectiveIndex, randomAdjectiveIndex];
